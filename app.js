5fd35f4be50801cf8d9dda6530648cc6578a57cd
samade747 <samad.e747@gmail.com>
2023-11-07 19:55:33 +0500

    6416540211+98431

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ const paragraph = "This is a paragraph. It is a very long, long paragraph. It co
 const para = document.querySelector('p');
 
 para.textContent = paragraph.slice(0, 10) + '...';
+
+const button = document.querySelector('button');
+const plus = document.querySelector('#plus');
+const minus = document.querySelector('#minus');
+
+button.setAttribute('onclick', 'showMore()');
+button.addEventListener('click', showMore);
+
